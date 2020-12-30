e649d7ce83ed9fef1ee94e76c910b09bd52117eb
KieranLitschel <kieran.litschel@hotmail.co.uk>
2020-12-30 12:04:31 +0000

    Added exception base class, and exception for unitialized model weights.

---
+++ xswem/exceptions.py
@@ -0,0 +1,20 @@
+DEFAULT_UNINITIALIZED_WEIGHTS_EXCEPTION_MESSAGE = "Model weights are not initialised. Train the model before calling " \
+                                                "an explain method."
+
+
+class XSWEMException(Exception):
+    """ Base class for exceptions in XSWEM """
+
+    def __init__(self, message):
+        self.message = message
+
+    def __str__(self):
+        return self.message
+
+
+class UninitializedWeightsException(XSWEMException):
+    """ Raised when a method requires the model weights to be initialized but they are uninitialized. """
+
+    def __init__(self, message=None):
+        message = message or DEFAULT_UNINITIALIZED_WEIGHTS_EXCEPTION_MESSAGE
+        super(UninitializedWeightsException, self).__init__(message)
