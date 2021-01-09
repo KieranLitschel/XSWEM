61445490c5816630d39129d8ba8c1fcd6735e4c1
KieranLitschel <kieran.litschel@hotmail.co.uk>
2021-01-09 14:18:30 +0000

    Added function to convert a pre-trained glove embeddings text file to an embedding_weights_map.

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,6 +1,7 @@
 import unittest
-from unittest.mock import Mock
-from xswem.utils import assert_layers_built
+import numpy as np
+from unittest.mock import Mock, MagicMock, patch, mock_open
+from xswem.utils import assert_layers_built, prepare_embedding_weights_map_from_glove
 from xswem.exceptions import UnbuiltLayersException
 
 
@@ -28,6 +29,27 @@ class TestUtils(unittest.TestCase):
         unbuilt_model = Model([mock_layer_built, mock_layer_unbuilt])
         with self.assertRaises(UnbuiltLayersException):
             unbuilt_model.get_output()
+
+    def test_prepare_embedding_weights_map_from_glove(self):
+        glove_file_path = "foo"
+        vocab = ["hello", "foo", "<unk>"]
+        read_data = "hello 1.1 0.2 3.3\n<unk> -1.1 -0.2 -3.3\n"
+        with patch('builtins.open', mock_open(read_data=read_data)):
+            with patch('xswem.utils.tqdm', new_callable=MagicMock()) as mock_tqdm:
+                mock_tqdm.__iter__.return_value = read_data.split("\n")
+                # test expected return with verbose off
+                embedding_weights_map = prepare_embedding_weights_map_from_glove(glove_file_path, vocab)
+                expected_embedding_weights_map = {
+                    "hello": np.array([1.1, 0.2, 3.3], dtype=np.float32),
+                    "<unk>": np.array([-1.1, -0.2, -3.3], dtype=np.float32),
+                }
+                self.assertCountEqual(embedding_weights_map.keys(), expected_embedding_weights_map.keys())
+                np.testing.assert_array_equal(embedding_weights_map["hello"], expected_embedding_weights_map["hello"])
+                np.testing.assert_array_equal(embedding_weights_map["<unk>"], expected_embedding_weights_map["<unk>"])
+                mock_tqdm().__iter__.assert_not_called()
+                # test verbose on
+                prepare_embedding_weights_map_from_glove(glove_file_path, vocab, verbose=1)
+                mock_tqdm().__iter__.assert_called_once()
 
 
 if __name__ == '__main__':
